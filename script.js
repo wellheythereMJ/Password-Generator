57d37139c26dad337431fe01f086a30ce71600fa
wellheythereMJ <abetterplace@outlook.com>
2024-01-14 10:29:41 -0700

    added characters to work with

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//placeholder array
+var finalPass = []
+
+//4 arrays of characters
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("")
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+var numbers = "1234567890".split("")
+var specials = "!@#$%^&*_+".split("")
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
